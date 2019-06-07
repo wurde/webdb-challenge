892a4c0d384b8ffeffcee11501ee736fdf670ee4
Andy Bettisworth <andy@accreu.com>
2019-06-07 11:11:56 -0500

    Add Project and Action models

---
+++ server/models/Action.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const db_client = require('../db/client')
+
+/**
+ * Define model
+ */
+
+class Action {
+  static async all() {
+    return []
+  }
+}
+
+/**
+ * Export model
+ */
+
+module.exports = Action
---
+++ server/models/Project.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const db_client = require('../db/client')
+
+/**
+ * Define model
+ */
+
+class Project {
+  static async all() {
+    return []
+  }
+}
+
+/**
+ * Export model
+ */
+
+module.exports = Project
