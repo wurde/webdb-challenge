6487e781ccfe4714d0252449f4c0e0cefc0302e4
Andy Bettisworth <andy@accreu.com>
2019-06-07 10:36:22 -0500

    Add helmet middleware

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@
  */
 
 const express = require('express')
+const helmet = require('helmet')
 
 /**
  * Constants
@@ -17,6 +18,12 @@ const port = process.env.PORT || 8080
  */
 
 const app = express()
+
+/**
+ * Middleware
+ */
+
+app.use(helmet())
 
 /**
  * Routes
