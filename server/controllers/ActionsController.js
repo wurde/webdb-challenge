45770dff98b39c88fe1aca76095f89499aef7f45
Andy Bettisworth <andy@accreu.com>
2019-06-07 11:21:04 -0500

    Copy and paste fn from projects to actions

--- server/controllers/ActionsController.js
+++ server/controllers/ActionsController.js
@@ -12,23 +12,58 @@ const Action = require('../models/Action')
 
 class ActionsController {
   static async all(req, res) {
-    res.sendStatus(200)
+    try {
+      let actions = await Action.all()
+
+      res.status(200).json(actions)
+    } catch(err) {
+      console.error(err)
+      res.status(500).json({ error: { message: 'Server error.' }})
+    }
   }
 
   static async create(req, res) {
-    res.sendStatus(200)
+    try {
+      let action = await Action.create(req.body)
+
+      res.status(201).json(action)
+    } catch(err) {
+      console.error(err)
+      res.status(500).json({ error: { message: 'Server error.' }})
+    }
   }
 
   static async find(req, res) {
-    res.sendStatus(200)
+    try {
+      let action = await Action.find(req.params.id)
+
+      res.status(200).json(action)
+    } catch(err) {
+      console.error(err)
+      res.status(500).json({ error: { message: 'Server error.' }})
+    }
   }
 
   static async update(req, res) {
-    res.sendStatus(200)
+    try {
+      let count = await Action.update(req.params.id, req.body)
+
+      res.status(200).json(count)
+    } catch(err) {
+      console.error(err)
+      res.status(500).json({ error: { message: 'Server error.' }})
+    }
   }
 
   static async remove(req, res) {
-    res.sendStatus(200)
+    try {
+      let count = await Action.remove(req.params.id)
+
+      res.status(200).json(actions)
+    } catch(err) {
+      console.error(err)
+      res.status(500).json({ error: { message: 'Server error.' }})
+    }
   }
 }
 
--- server/models/Action.js
+++ server/models/Action.js
@@ -12,7 +12,27 @@ const db_client = require('../db/client')
 
 class Action {
   static async all() {
-    return []
+    return await db_client('actions')
+  }
+
+  static async find(id) {
+    return db_client('actions').where({ id }).first()
+  }
+
+  static async create(project) {
+    return await db_client('actions').insert(project)
+  }
+
+  static async update(id, project) {
+    return db_client('actions')
+      .where({ id }).first()
+      .update(project)
+  }
+
+  static async remove(id) {
+    return await db_client('actions')
+      .where({ id: id })
+      .del()
   }
 }
 
