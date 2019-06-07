cb2d2046c8b6147dc507cc1eff6eed53933c74ca
Andy Bettisworth <andy@accreu.com>
2019-06-07 10:52:30 -0500

    Define create_projects and create_actions migrations

--- server/db/migrations/20190607104335_create_actions.js
+++ server/db/migrations/20190607104335_create_actions.js
@@ -1,8 +1,20 @@
+'use strict'
 
 exports.up = function(knex, Promise) {
-  
-};
+  return knex.schema.createTable('actions', (table) => {
+    table.increments('id')
+    table.integer('projects_id').notNullable()
+    table.text('description')
+    table.text('notes')
+    table.datetime('completed_at')
+    table.timestamps(true, true)
+
+    table.foreign('projects_id').references('id').inTable('projects')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  })
+}
 
 exports.down = function(knex, Promise) {
-  
-};
+  return knex.schema.dropTableIfExists('actions')
+}
